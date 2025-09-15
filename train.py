98fee10d64d08de848b7d762ccaa41ce308a7cb0
Connor <94580584+Bluish1251@users.noreply.github.com>
2025-09-14 22:26:59 -0400

    Delete train.py

--- train.py
+++
@@ -1,4 +0,0 @@
-# Training script for YOLOv8n on SUAS dataset
-from ultralytics import YOLO
-model = YOLO('yolov8n.pt')
-model.train(data='suas16.yaml', epochs=50, imgsz=640, batch=16, name='yolo8n_suas')
